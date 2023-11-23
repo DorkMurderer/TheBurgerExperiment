1638630289c019dfeed3a81a2e29f1785a8e87ad
DorkMurderer <78510253+DorkMurderer@users.noreply.github.com>
2023-11-23 01:02:27 -0800

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,116 @@
+let btn = document.getElementById("btn")
+btn.addEventListener("click", function(){
+    location.reload()
+})
+
+let currMoleTile
+let currPlantTile
+let score =  0
+let gameOver = false
+
+
+window.onload = function() {
+    setGame()
+}
+
+function setGame() {
+    for (let i = 0; i < 9; i++){
+        let tile = document.createElement("div")
+        tile.id = i.toString()
+        tile.addEventListener("click", selectTile)
+        document.getElementById("board").appendChild(tile)
+    }
+
+    setInterval(setMole, 550)
+    setInterval(setPlant, 1000)
+}
+
+function getRandomTile() {
+
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
+}
+
+function setMole(){
+
+    if (gameOver){
+        return
+    }
+     
+
+    if (currMoleTile){
+        currMoleTile.innerHTML = ""
+    }
+
+    let mole = document.createElement("img")
+    mole.src = 'smileborger.png.webp'
+
+    let num = getRandomTile()
+    if (currPlantTile && currPlantTile.id == num) {
+        return
+    }
+
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+
+    if (gameOver){
+        return
+    }
+    if (currPlantTile){
+        currPlantTile.innerHTML = ""
+    }
+
+    let plant = document.createElement("img")
+    plant.src = "soy.png"
+    
+    let num = getRandomTile()
+
+    if (currMoleTile && currMoleTile.id == num){
+        return;
+    }
+    currPlantTile = document.getElementById(num)
+    currPlantTile.appendChild(plant)
+}
+
+
+
+
+function selectTile() {
+
+        if (gameOver){
+
+           
+            return
+
+            
+            
+            
+            
+        }
+
+      
+       if (this == currMoleTile) {
+          score += 10
+          document.getElementById("score").innerText = score.toString()
+       }
+         else if (this == currPlantTile) {
+          
+
+            document.getElementById("score").innerText = "GAME OVER  " + score.toString()
+       
+            gameOver = true
+            let btn = document.getElementById("btn")
+            btn.classList.remove("hide")
+         
+         }
+
+}
+
+function playSound(audioName){
+    let audio = new Audio(audioName)
+     audio.loop = false 
+     audio.play()
+}
